e028025648c34346f515df98ec5b3868f3c4af78
Johannes Enevoldsen <johannes.ne@gmail.com>
2021-06-09 16:00:19 +0200

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Cardiac output
+#'
+#' Cardiac output by two methods, RV and IS, for 12 subjects
+#'
+#' @format A data frame with 60 rows and 3 variables:
+#' \describe{
+#'   \item{sub}{Subject}
+#'   \item{rv}{Cardiac output,  radionuclide ventriculography (RV)}
+#'   \item{ic}{Cardiac output,  impedance cardiography (IC)}
+#' }
+#'
+#' @source \url{https://www-users.york.ac.uk/~mb55/datasets/datasets.htm}
+#'
+#' @references \cite{Bland JM, Altman DG. (1999) Measuring agreement in method comparison studies. Statistical Methods in Medical Research 8, 135-160.}
+#' (data provided by Dr LS Bowling)
+#'
+"CO"
