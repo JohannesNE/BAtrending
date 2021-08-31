13800d463838e302b47b695bb6a879ab8e1841d5
Johannes Enevoldsen <johannes.ne@gmail.com>
2021-08-31 12:48:29 +0200

    calc confint

--- R/calc_stats.R
+++ R/calc_stats.R
@@ -9,60 +9,89 @@
 #' @export
 #'
 #' @examples compare_methods(CO, ref_col = "rv", alt_col = "ic", id_col = "sub")
-compare_methods <- function(df, ref_col, alt_col, id_col) {
+compare_methods <- function(df, ref_col, alt_col, id_col, REML = TRUE) {
   if (!is.data.frame(df)) stop("df must be of class data.frame")
 
   df$diff <- df[[alt_col]] - df[[ref_col]]
   df$mean <- (df[[alt_col]] + df[[ref_col]]) / 2
 
-  diff_model <- lme4::lmer(as.formula(paste0("diff ~ 1 + (1 | ", id_col, ")")), data = df)
+  diff_model <- lme4::lmer(as.formula(paste0("diff ~ 1 + (1 | ", id_col, ")")),
+                           REML = REML, data = df)
 
   # Extract variance components
-  varCorr_df <- as.data.frame(lme4::VarCorr(diff_model))
-  sd_components <- varCorr_df[["sdcor"]]
-  names(sd_components) <- varCorr_df[["grp"]]
-
-  stopifnot(length(sd_components) == 2)
+  BA_stats <- calc_BA_stats_from_model(diff_model)
 
   structure(
     list(
     data = df,
     model = diff_model,
-    var = list(inter_sd = sd_components[1],
-              intra_sd = sd_components[2],
-              total_sd = sqrt(sum(sd_components^2))
-              )
+    BA_stats = as.list(BA_stats)
   ),
     class = "ba_analysis"
   )
 }
 
-# Print method
+#' Calculate confidence interval
+#' @export
+confint.ba_analysis <- function(x, level = 0.95, nsim = 2000) {
+  message(sprintf("Creating %i bootstrap samples", nsim))
+  lme4::confint.merMod(x$model,
+                       method="boot",
+                       FUN = calc_BA_stats_from_model,
+                       level = level,
+                       nsim = nsim,
+                       .progress = 'txt')
+}
+
+#' Print method
 #' @export
 print.ba_analysis <- function(x) {
   ops <- options(digits = 3)
-  on.exit(ops)
+  on.exit(options(ops))
 
   n_obs <- x$model@devcomp$dims[["n"]]
   n_sub <- nlevels(x$model@flist[[1]])
-  mean_bias <- lme4::fixef(x$model)
 
   cat(sprintf("%i paired measurements in %i subjects\n\n", n_obs, n_sub))
 
 
 
   cat("Variance components:\n")
-  cat("Interindividual variance (SD):", x$var$inter_sd, "\n")
-  cat("Intraindividual variance (SD):", x$var$intra_sd, "\n")
-  cat("Total variance (SD)          :", x$var$total_sd, "\n\n")
+  cat("Interindividual variance (SD):", x$BA_stats$sd.id, "\n")
+  cat("Intraindividual variance (SD):", x$BA_stats$sd.residual, "\n")
+  cat("Total variance (SD)          :", x$BA_stats$sd.combined, "\n\n")
 
-  cat("Mean difference (alt - ref):", mean_bias, "\n")
+  cat("Mean difference (alt - ref):", x$BA_stats$bias, "\n")
   cat(sprintf("Limits of Agreement (95%%)  : [%.3f; %.3f]",
-              mean_bias - 2 * x$var$total_sd,
-              mean_bias + 2 * x$var$total_sd))
+              x$BA_stats$bias - 2 * x$BA_stats$sd.combined,
+              x$BA_stats$bias + 2 * x$BA_stats$sd.combined))
 
   invisible(x)
 }
 
 # Autoplot method
 
+calc_BA_stats_from_model <- function(model) {
+
+  bias <- lme4::fixef(model) # Get intercept from model
+  stopifnot(length(bias) == 1) # Test
+  bias <- bias[[1]]
+
+
+  # Get variance components (SD)
+  varCorr_df <- as.data.frame(lme4::VarCorr(model))
+  sd_components <- varCorr_df[["sdcor"]]
+  names(sd_components) <- varCorr_df[["grp"]]
+
+  stopifnot(length(sd_components) == 2) # Test
+
+  sd_combined <- sqrt(sum(sd_components^2))
+
+  c(bias = bias,
+    sd.id = unname(sd_components[1]),
+    sd.residual = unname(sd_components[2]),
+    sd.combined = sd_combined,
+
+    loa.lwr = bias - 2*sd_combined,
+    loa.upr = bias + 2*sd_combined)
+}
