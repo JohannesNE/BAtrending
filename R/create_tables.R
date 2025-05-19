6bd5f50331838da0be6fcc843614e75e7a2345a8
JohannesNE <johannes.ne@gmail.com>
2025-05-19 10:39:55 +0200

    update create tables

--- R/create_tables.R
+++ R/create_tables.R
@@ -14,7 +14,23 @@ ba_stat_labels <- c(
   
 )
 
+
+#' Create a table with the results of the Bland-Altman analysis
+#'
+#' @description
+#' Uses {tinytable}.
+#'
+#' @param ba_obj An object created with `BAtrending::compare_methods()`. 
+#' @param decimals A single numeric value specifying the number of decimal places for estimates and confidence intervals.
+#' @param decimals_pct A single numeric value specifying the number of decimal places for percentage statistics.
+#'
+#' @returns
+#' A {tinytable} table of the estimates from the Bland-Altman analysis.
+#'
+#' @export
 BA_table <- function(ba_obj, decimals = 2, decimals_pct = 1) {
+  assert_BA_obj(ba_obj)
+
   ba_est <- as.data.frame(ba_obj)
   ba_est$label <- ba_stat_labels[ba_est$stat]
 
@@ -31,29 +47,36 @@ BA_table <- function(ba_obj, decimals = 2, decimals_pct = 1) {
 
   # Add ± to relevant stats
   ba_est$est_ci <- ifelse(ba_est$stat %in% c("change.loa", "trending.precision"),
-    paste0("±", est_ci),
-    est_ci
+    paste0("±", ba_est$est_ci),
+    ba_est$est_ci
   )
 
-  loa_group_label <- list("Limits of agreement (95%)" = which(ba_table$variable == "loa.upr"))
-
+  loa_group_label <- list("Limits of agreement (95%)" = which(ba_est$stat == "loa.upr"))
+  
   tab_footnotes <- list(
     "a" = list(
-      i = which(ba_table$variable == "trending.precision"),
+      i = which(ba_est$stat == "trending.precision"),
       j = 1,
       text = "Trending precision (95%) = 2 * Within subject variation (SD)."
     ),
     "b" = list(
-      i = which(ba_table$variable == "change.loa"),
+      i = which(ba_est$stat == "change.loa"),
       j = 1,
       text = "Change limits of agreement (95%) = √2 * Trending precision (95%)."
     )
   )
+  
+  est_label <- if(is.numeric(ba_est$ci.upr)) {
+    "Estimate [95% CI]"
+  } else {
+    "Estimate"
+  }
 
+  # Select relevant variables for table
   ba_table_df <- ba_est[, c("label", "est_ci")]
   ba_table_tt <- tinytable::tt(ba_table_df, notes = tab_footnotes)
   ba_table_tt <- tinytable::group_tt(ba_table_tt, i = loa_group_label, indent = 0)
-  names(ba_table_tt) <- c("", "Estimate [95% CI]")
+  names(ba_table_tt) <- c("", est_label)
 
   ba_table_tt
 }
@@ -62,10 +85,13 @@ BA_table <- function(ba_obj, decimals = 2, decimals_pct = 1) {
 format_est_ci <- function(est, lwr = NULL, upr = NULL, fmt_pct = FALSE, decimals = 2, decimals_pct = 1) {
 
   # Argument checks
-  if (!is.numeric(est)) stop("`est` must be numeric.", call. = FALSE)
-  if (length(est) == 0) stop("`est` cannot be empty.", call. = FALSE)
+  if (!is.numeric(est)) cli::cli_abort("{.var est} must be numeric.")
+  if (length(est) == 0) cli::cli_abort("{.arg est} cannot be empty.")
   if (xor(is.null(lwr), is.null(upr))) {
-      stop("If one of `lwr` or `upr` is provided, the other must also be. Or both can be NULL.", call. = FALSE)
+      cli::cli_abort(
+  "If one of {.arg lwr} or {.arg upr} is provided, the other must also be.
+   Or both can be NULL."
+)
   }
 
   n_est <- length(est)
--- tests/testthat/test-test_tables.R
+++ tests/testthat/test-test_tables.R
@@ -3,8 +3,16 @@ test_that("format_single_est_ci works", {
   expect_equal(format_single_est_ci(est = 1, decimals = 1, fmt_pct = TRUE), "100.0 %")
 })
 
-testthat("format_est_ci works", {
+test_that("format_est_ci works", {
   expect_equal(format_est_ci(1), "1.00")
   expect_equal(format_est_ci(1:2, 1:2, 1:2, fmt_pct = c(TRUE, FALSE)), c("100.0 [100.0; 100.0] %", "2.00 [2.00; 2.00]"))
   expect_error(format_est_ci(1, 1))
+})
+
+test_that("BA_table works without CI", {
+  expect_snapshot(BA_table(comp_co))
+})
+
+test_that("BA_table works with CI", {
+  expect_snapshot(BA_table(comp_co_w_ci))
 })
